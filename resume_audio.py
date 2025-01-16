7da9c377a4e14cf44294ee58f83d86249c431431
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2025-01-16 15:54:09 -0300

    feat: Adding audio file transcription func.

---
+++ resume_audio.py
@@ -0,0 +1,31 @@
+"""
+Entry point file. Usage: python resume_audio.py <audio_path>
+"""
+
+from sys import argv, exit
+from whisper import load_model
+
+
+
+def main():
+    if len(argv) < 2:
+        print("Usage: python resume_audio.py <audio_path>")
+        exit(1)
+    elif len(argv) > 2:
+        print("WARNNING: You passed more than one argument, only the first one will be used.")
+
+    audio_path = argv[1]
+
+    audio_transcript = transcribe_audio(audio_path)
+
+    print(audio_transcript)
+
+
+def transcribe_audio(audio_path: str) -> str:
+    w_model = load_model("small")
+
+    return w_model.transcribe(audio_path, fp16=False)["text"]
+
+
+if __name__ == "__main__":
+    main()
