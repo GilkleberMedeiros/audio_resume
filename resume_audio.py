4ce3045db0ac82228ae027d0330d43245f0cd6b1
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2025-01-16 16:59:48 -0300

    feat: adding fake audio feature.

--- resume_audio.py
+++ resume_audio.py
@@ -16,10 +16,23 @@ def main():
         "audio_path", 
         help="Path to the audio file to be resumed."
     )
+    parser.add_argument(
+        "-f", "--fake-transcription",
+        dest="fake_transcription",
+        help="If present espects <audio_path> to be a .txt file to use as transcription.",
+        action="store_true",
+    )
 
     args = parser.parse_args()
 
-    audio_transcript = transcribe_audio(args.audio_path)
+    if args.fake_transcription:
+        if not args.audio_path.endswith(".txt"):
+            raise ValueError("When using -f, --fake-transcription, <audio_path> must be a .txt file.")
+        
+        with open(args.audio_path, "r") as f:
+            audio_transcript = f.read()
+    else:
+        audio_transcript = transcribe_audio(args.audio_path)
 
     print(audio_transcript)
 
